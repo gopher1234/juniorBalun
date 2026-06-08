4bef1433871fd60e71043045d02ac5d82e3b7652
Artem <h86312712@gmail.com>
2026-06-08 06:30:43 +0300

    http server

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+)
+
+var Commit = func() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				return setting.Value
+			}
+		}
+	}
+
+	return ""
+}()
+
+func debugInfo(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Println(Commit)
+}
 
 func main() {
-	fmt.Println("asdf")
+	http.HandleFunc("/debug/info", debugInfo)
+
+	http.ListenAndServe(":8090", nil)
 }
